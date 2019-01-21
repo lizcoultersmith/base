223b8be7cf869126e134d22cb2010ae18cee0a00
Liz Coulter-Smith <liz@liz-macbook.local>
2019-01-21 13:40:36 +0000

    140

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
+
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "images/lizzie.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic").src = "images/me_2017bw.jpg";
+  }
+/*
+
+$(".go-to-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    }, 1000);
+});
+
+
+
+
 function changePic() {
     document.getElementById("profilePic").src = "images/me_2017bw.jpg";
   }
   
   function normalPic() {
-    document.getElementById("profilePic").src = "images/lizzie.jpg";
+    document.getElementById("profilePic").src = "images/lizzie..jpg";
   }
+  */
