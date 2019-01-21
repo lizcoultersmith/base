433183d2b9496373578e3185789597052ea080eb
Liz Coulter-Smith <liz@liz-macbook.local>
2019-01-21 13:26:47 +0000

    126

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function changePic() {
+    document.getElementById("profilePic").src = "images/me_2017bw.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic").src = "images/lizzie.jpg";
+  }
